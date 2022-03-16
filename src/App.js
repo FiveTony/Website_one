df2a47b326f254aa6c41f0ebbb7b66daa12b85b9
FiveTony <a.malynov@wim.agency>
2022-03-16 23:14:46 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar/'
+import { BrowserRouter } from 'react-router-dom'
+import Sidebar from './components/Sidebar';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Sidebar />
+      <Navbar />
+    </BrowserRouter>
+  );
+}
+
+export default App;
